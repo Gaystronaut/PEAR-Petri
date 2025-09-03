e8c3e582c3b2f723a88f1aab0c67fb96643b7687
GAY2025 <zhuhongwei2025@gmail.com>
2025-09-03 17:00:58 +0800

    Add files via upload

---
+++ src/util/sum_tree.py
@@ -0,0 +1,72 @@
+import numpy
+import random
+class SumTree:
+    """
+    Implementation of the Sum Tree data structure
+    Taken from
+    https://github.com/jaromiru/AI-blog/blob/master/SumTree.py
+    """
+    write = 0
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.tree = numpy.zeros(2 * capacity - 1)
+        self.data = numpy.zeros(capacity, dtype=object)
+
+    def _propagate(self, idx, change):
+        parent = (idx - 1) // 2
+
+        self.tree[parent] += change
+
+        if parent != 0:
+            self._propagate(parent, change)
+
+    def _retrieve(self, idx, s):
+        left = 2 * idx + 1
+        right = left + 1
+
+        if left >= len(self.tree):
+            return idx
+
+        if s <= self.tree[left]:
+            return self._retrieve(left, s)
+        else:
+            return self._retrieve(right, s - self.tree[left])
+
+    def total(self):
+        return self.tree[0]
+
+    def add(self, p, data):
+        idx = self.write + self.capacity - 1
+
+        self.data[self.write] = data
+        self.update(idx, p)
+
+        self.write += 1
+        if self.write >= self.capacity:
+            self.write = 0
+
+
+    def random_add(self, p, data):
+        random_idx = random.randint(0, self.capacity - 1)
+
+        self.data[random_idx] = data
+
+        # 计算在树中的索引
+        tree_idx = random_idx + self.capacity - 1
+
+        self.update(tree_idx, p)
+
+    def update(self, idx, p):
+        change = p - self.tree[idx]
+
+        self.tree[idx] = p
+        self._propagate(idx, change)
+
+    def get(self, s):
+        idx = self._retrieve(0, s)
+        data_idx = idx - self.capacity + 1
+
+        return idx, self.tree[idx], self.data[data_idx]
+    
+    def count(self):
+        return self.write if self.write < self.capacity else self.capacity
